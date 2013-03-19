14f10b89907795783eb0b6250cb82bd7ae9084d6
Vagrant User <vagrant@debian-606-x64.vm>
2013-03-19 12:08:06 +0000

    makesymlinks.sh mis à jour

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_aliases bash_profile bashrc inputrc tmux.conf vimrc"    # list of files/folders to symlink in homedir
+files="bash_aliases bash_profile inputrc tmux.conf vimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -30,30 +30,13 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
-function install_zsh {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/michaeljsmalley/oh-my-zsh.git
-    fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        sudo apt-get install zsh
-        install_zsh
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
-}
+cat <<EOF >> ~/.bashrc
 
-install_zsh
+# sroccaserra/dotfiles
+
+if [[ -f ~/dotfiles/bashrc ]]
+then
+    source ~/dotfiles/bashrc
+fi
+EOF
+
