67848c31113cbc20a38adce23218a3fbb312305c
sroccaserra <sroccaserra@yahoo.com>
2013-03-20 12:56:06 +0100

    Update makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_aliases bash_profile inputrc tmux.conf vimrc"    # list of files/folders to symlink in homedir
+files="bash_aliases bash_profile inputrc noserc tmux.conf vimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
