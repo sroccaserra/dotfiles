d9108fd92975be23081428afdd41cb0521c6354c
sroccaserra <sroccaserra@yahoo.com>
2013-04-09 13:15:10 +0300

    Update install_debian_dev_tools.sh

--- install_debian_dev_tools.sh
+++ install_debian_dev_tools.sh
@@ -6,7 +6,7 @@ aptitude install -y linux-headers-$(uname -r) build-essential \
         zlib1g-dev libssl-dev libreadline5-dev \
         curl zip unzip vim rake ntp libevent-dev rlwrap \
         g++ bison flex libncurses-dev libssh-dev cmake \
-        git colormake colordiff dos2unix lintian \
+        git colormake colordiff dos2unix apt-file lintian linux-tools \
         mysql-client \
         python-dev python-nose python-unittest2 python-paver \
         python-mysqldb python-sqlalchemy virtualenvwrapper
