cf716364b5691cbb0912287bca52e6942afa304d
sroccaserra <sroccaserra@yahoo.com>
2013-04-09 20:17:39 +0300

    Update install_debian_dev_tools.sh
    Added libmysqlclient-dev 

--- install_debian_dev_tools.sh
+++ install_debian_dev_tools.sh
@@ -7,7 +7,7 @@ aptitude install -y linux-headers-$(uname -r) build-essential \
         curl zip unzip vim rake ntp libevent-dev rlwrap \
         g++ bison flex libncurses-dev libssh-dev cmake \
         git colormake colordiff dos2unix apt-file lintian linux-tools \
-        mysql-client \
+        mysql-client libmysqlclient-dev \
         python-dev python-nose python-unittest2 python-paver \
         python-mysqldb python-sqlalchemy virtualenvwrapper
 
