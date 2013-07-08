d14b01d48403cefb4650a4a95beb8fa2acc0bb99
sroccaserra <sroccaserra@yahoo.com>
2013-07-08 21:39:44 +0200

    Correction de la compilation de Vim

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -74,17 +74,18 @@ sed -n '/^### Shared with Byobu ###$/,$ p' tmux.conf > ~/.byobu/.tmux.conf
 ## Vim
 if [[ ! -f /usr/local/bin/vim ]]
 then
-    cd /usr/local/src; \
+    mkdir -p ~/developer
+    cd ~/developer; \
         hg clone https://code.google.com/p/vim/; \
         cd vim; \
         ./configure --enable-rubyinterp --enable-pythoninterp --enable-luainterp --with-features=HUGE; \
         colormake; \
-        colormake install
+        sudo colormake install
 
-    update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 200 --slave /usr/share/man/man1/editor.1 editor.1 /usr/local/share/man/man1/vim.1
-    update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 200 --slave /usr/share/man/man1/vi.1 vi.1 /usr/local/share/man/man1/vim.1
-    update-alternatives --set editor /usr/local/bin/vim
-    update-alternatives --set vi /usr/local/bin/vim
+    sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 200 --slave /usr/share/man/man1/editor.1 editor.1 /usr/local/share/man/man1/vim.1
+    sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 200 --slave /usr/share/man/man1/vi.1 vi.1 /usr/local/share/man/man1/vim.1
+    sudo update-alternatives --set editor /usr/local/bin/vim
+    sudo update-alternatives --set vi /usr/local/bin/vim
 fi
 
 mkdir -p ~/.vim/colors
