cce726290bc531699566e6e77c74e742d330d6d2
sra <sra@voltalis.com>
2013-07-16 12:38:45 +0200

    Customisation Emacs

--- custom.el
+++ custom.el
@@ -3,7 +3,18 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(evil-leader/leader ","))
+ '(abbrev-mode t)
+ '(column-number-mode t)
+ '(cursor-in-non-selected-windows nil)
+ '(delete-old-versions t)
+ '(evil-leader/leader ",")
+ '(global-auto-revert-mode t)
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
+ '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/"))))
+ '(pop-up-windows nil)
+ '(recentf-max-saved-items 1024)
+ '(recentf-mode t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
