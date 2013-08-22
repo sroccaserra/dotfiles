a231228e2b9256d43dcb10298820945ff5ba4d17
sroccaserra <sroccaserra@yahoo.com>
2013-08-22 23:47:41 +0200

    Correction du fichier install.bat (gestion des espaces dans %HOME%)

--- install.bat
+++ install.bat
@@ -11,27 +11,27 @@ if not defined GIT (
     echo Please add Git to your path.
 )
 
-if not exist %HOME%\.vim\bundle\vundle (
-    git clone https://github.com/gmarik/vundle.git %HOME%/.vim/bundle/vundle
+if not exist "%HOME%\.vim\bundle\vundle" (
+    git clone https://github.com/gmarik/vundle.git "%HOME%\.vim\bundle\vundle"
 )
 
-if not exist %HOME%\.vimrc (
-    echo source ~/dotfiles/vimrc > %HOME%\.vimrc
+if not exist "%HOME%\.vimrc" (
+    echo source ~/dotfiles/vimrc > "%HOME%\.vimrc"
 )
 
 if not exist %HOME%\.emacs (
-    echo (load-file "~/dotfiles/emacs"^) > %HOME%\.emacs
+    echo (load-file "~/dotfiles/emacs"^) > "%HOME%\.emacs"
 )
 
-if not exist %HOME%\.ssh (
-    if exist %HOME%\Dropbox\.ssh (
-        mklink /D %HOME%\.ssh %HOME%\Dropbox\.ssh
+if not exist "%HOME%\.ssh" (
+    if exist "%HOME%\Dropbox\.ssh" (
+        mklink /D "%HOME%\.ssh" "%HOME%\Dropbox\.ssh"
     )
 )
 
 for %%p in (emacs smart-tab) do (
     if not exist "%HOME%\developer\%%p" (
-        git clone "git@github.com:sroccaserra/%%p.git" "%HOME%/developer/%%p"
+        git clone "git@github.com:sroccaserra/%%p.git" "%HOME%\developer\%%p"
     ) else (
         echo %%p ok.
     )
@@ -50,8 +50,9 @@ for %%X in (emacs.exe) do (set EMACS=%%~$PATH:X)
 if not defined EMACS (
     echo Suggestion: You should add Emacs to your path.
 ) else (
-    for %%E in (%EMACS%) do (
-        copy "%PWD%\emacsw.bat" "%%~dpE"
+    for %%E in ("%EMACS%") do (
+        echo copy "%PWD%emacsw.bat" "%%~dpE"
+        copy "%PWD%emacsw.bat" "%%~dpE"
     )
 )
 
