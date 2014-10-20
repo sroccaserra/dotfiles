4e2a4adf4b867774e64c546f7ce6bd25eb3bf276
sroccaserra <sroccaserra@yahoo.com>
2014-10-20 18:16:37 +0200

    Added rcconf to debian tools

--- install_debian_dev_tools.sh
+++ install_debian_dev_tools.sh
@@ -4,7 +4,7 @@ aptitude update -y
 
 aptitude install -y linux-headers-$(uname -r) build-essential \
         zlib1g-dev libssl-dev libreadline5-dev \
-        curl zip unzip vim rake ntp libevent-dev rlwrap \
+        curl zip unzip vim rake ntp libevent-dev rlwrap rcconf \
         g++ bison flex libncurses-dev libssh-dev cmake \
         git colormake colordiff dos2unix apt-file lintian linux-tools \
         mysql-client libmysqlclient-dev \
