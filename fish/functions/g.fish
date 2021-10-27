9680c707a3c89fe89481687f874771e08e0d5193
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-10-27 13:17:35 +0200

    Add result of fzf in clipboard in g function

--- fish/functions/g.fish
+++ fish/functions/g.fish
@@ -1,6 +1,7 @@
 function g --description='open fzf then inserts selection in commandline'
     git ls-files | fzf | read foo
     if [ "$foo" ]
+        echo -n "$foo" | pbcopy
         commandline -i "$foo"
     end
 end
