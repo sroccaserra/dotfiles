c54a7143cacce267f06b4545a9ac4b491025cc47
sroccaserra <sroccaserra@yahoo.com>
2022-09-05 19:45:57 +0200

    Switch to fzf for Vim find_file_or_test_file()

--- nvim/lua/find_file_or_test_file.lua
+++ nvim/lua/find_file_or_test_file.lua
@@ -1,17 +1,15 @@
-local telescope = require 'telescope.builtin'
-
 local function find_file_or_test_file()
   local filename_with_no_ext = vim.fn.expand('%:r'):gsub('.*/', '')
   local test_file_pattern = '[-_]?[tT]est'
 
   local fuzzy_query
   if filename_with_no_ext:find(test_file_pattern) then
-    fuzzy_query = filename_with_no_ext:gsub(test_file_pattern, '')
+    fuzzy_query = "'" .. filename_with_no_ext:gsub(test_file_pattern, '')
   else
-    fuzzy_query = filename_with_no_ext .. "test"
+    fuzzy_query = "'" .. filename_with_no_ext .. " 'test"
   end
 
-  telescope.find_files({ default_text = fuzzy_query })
+  vim.fn['fzf#vim#files']('.', { source = "rg -l ''", options = '--query "' .. fuzzy_query ..'"' })
 end
 
 return {
