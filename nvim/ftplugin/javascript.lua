eacd8184bc3a9b273cb6cc57727713d78f92116b
sroccaserra <sroccaserra@yahoo.com>
2022-09-05 12:12:58 +0200

    Extract nvim find_file_or_test_file Lua function

--- nvim/ftplugin/javascript.lua
+++ nvim/ftplugin/javascript.lua
@@ -1,18 +1,4 @@
-local telescope = require 'telescope.builtin'
-
-local function find_file_or_test_file()
-  local filename_with_no_ext = vim.fn.expand('%:r'):gsub('.*/', '')
-  local test_file_pattern = '[-_]?[tT]est'
-
-  local fuzzy_query
-  if filename_with_no_ext:find(test_file_pattern) then
-    fuzzy_query = filename_with_no_ext:gsub(test_file_pattern, '')
-  else
-    fuzzy_query = filename_with_no_ext .. "test"
-  end
-
-  telescope.find_files({ default_text = fuzzy_query })
-end
+local find_file_or_test_file = require('find_file_or_test_file').fn
 
 local opts = { noremap = true, silent = true, buffer = true }
 vim.keymap.set('n', '<leader>a', find_file_or_test_file, opts)
