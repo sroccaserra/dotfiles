4a5a9230b63276ecc1743171e3e7bd7f82332664
sroccaserra <sroccaserra@yahoo.com>
2022-10-08 13:05:30 +0200

    Add Rust lsp config (Neovim)

--- nvim/lua/my_lsp_config.lua
+++ nvim/lua/my_lsp_config.lua
@@ -68,3 +68,12 @@ require 'lspconfig'.sumneko_lua.setup {
     },
   },
 }
+
+require('lspconfig')['rust_analyzer'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+
+    settings = {
+      ["rust-analyzer"] = {}
+    }
+}
