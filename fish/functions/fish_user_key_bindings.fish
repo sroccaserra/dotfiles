8b8a4ba62b8c0f75b51ee17f4bd04575db4c6dd0
sroccaserra <sroccaserra@yahoo.com>
2024-06-26 08:49:22 +0200

    f Raccourci fish pour insérer la date du jour

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,4 @@
+# Use fish_key_reader to find key names
 function fish_user_key_bindings
     if test ~/.config/fish/functions/fish_user_key_bindings.fish
         source ~/.config/fish/functions/fish_user_key_bindings.fish
@@ -5,5 +6,6 @@ function fish_user_key_bindings
     bind \e\eOA 'history-token-search-backward'
     bind \cX\cE 'edit_command_buffer'
     bind \cG 'g'
-    bind \cc 'commandline ""'
+    bind \cC 'commandline ""'
+    bind \cN 'commandline -i (date -I)'
 end
