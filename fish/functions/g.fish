37fd04b2490b82f28b8a795a2acf17631ce47caf
sroccaserra <sroccaserra@yahoo.com>
2024-06-26 08:40:16 +0200

    f Utilise rg en priorité pour lister les fichiers

--- fish/functions/g.fish
+++ fish/functions/g.fish
@@ -1,11 +1,13 @@
 function g --description='open fzf then inserts selection in commandline'
-    if [ (git rev-parse --git-dir 2> /dev/null) ]
-        git ls-files | fzf | read foo
+    if type -q rg
+        rg --files | fzf | read file
+    else if [ (git rev-parse --git-dir 2> /dev/null) ]
+        git ls-files | fzf | read file
     else
-        fzf | read foo
+        fzf | read file
     end
-    if [ "$foo" ]
-        echo -n "$foo" | pbcopy
-        commandline -i "$foo"
+    if [ "$file" ]
+        echo -n "$file" | pbcopy
+        commandline -i "$file"
     end
 end
